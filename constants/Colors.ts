5179b3e07a721ff6e140b57e138db566d7ea8f1e
hcy6021 <81142151+hcy6021@users.noreply.github.com>
2022-07-24 19:43:46 +0900

    Create a color pallete

---
+++ constants/Colors.ts
@@ -0,0 +1,15 @@
+export default {
+    white: '#FFFEFE',
+    black: '#1C2938',
+    lightGray: '#F6F6F6',
+    darkGray: '#D9D9D9',
+    deepGray: '#8B8B8B',
+    beige: '#FFF7ED',
+    yellow: '#FED06E',
+    pink: '#FF869E',
+    skyblue: '#7FB5FF',
+    navy: '#1F4690',
+    purple: '#CA82FF',
+    green: '#95CD41'
+  };
+  
